01aff5b4825359f75350c9a5bdc030fe5680f4ec
prmcsep103 <147101858+prmcsep103@users.noreply.github.com>
2024-01-26 13:12:16 +0000

    commit 2

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,6 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Jay");
-        System.out.println("Hi How are you all!!")
+        System.out.println("Jay What's up?");        
     }
 }
